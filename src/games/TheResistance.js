68b3d6daf5f25914fa485f8714d8573ddb32fe08
Alasdair MacLeod <alasdair.macleod@gmail.com>
2014-09-20 12:02:49 +0100

    Added more states to TheResistance game file

--- src/games/TheResistance.js
+++ src/games/TheResistance.js
@@ -4,6 +4,7 @@ var Resistance = function()
 	this.state = 0;
 	this.players = null;
 	this.characters = []; 
+	this.teamLeader = -1;
 
 	//State functions
 	var init = {name: "init", func: function(game){
@@ -25,7 +26,7 @@ var Resistance = function()
 			case 9:
 				spies = 3;
 				break;
-			case 10:
+			case 10:jm 
 				spies = 4;
 				break;
 		}
@@ -34,7 +35,7 @@ var Resistance = function()
 		{
 			if(spies > 0)
 			{
-				game.characters.push("spy");
+				game.characters.push("Spy");
 				spies--; 
 			}
 			else
@@ -43,33 +44,80 @@ var Resistance = function()
 			}
 		}
 
-		game.characters.shuffle()
-
-		for(var i = 0; i < game.players.length; i++)
-		{
-			
-		}
-
+		game.characters.shuffle();
 		game.state++;
-	}}
+	}};
 
 	var dealCharacters = {name: "dealCharacters", func: function(game){
 
+		//Tell everyone their roles
+		for(var i = 0; i < game.players.length; i++)
+		{
+			game.players[i].side = game.characters[i];
+			game.players[i].socket.emit('chat message', 'TheGame', 'You are: '+game.players[i].side);
+		}
+
+		game.state++;
 	}}; 
 
 	var revelSpies = {name: "revelSpies", func: function(game){
+		var spies = [];
+		var i = 0;
 
+		//Notify the spies
+		for( i = 0; i < game.players.length; i++)
+		{
+			if(game.players[i].side == "Spy" )
+			{
+				spies.push(game.players[i]);
+			}
+		}
+
+		var msg = "Spies: ";
+
+		//Send the list of spies to the players
+		for(i = 0; i < spies.length; i++)
+		{
+			msg += spies[i].name+ " ";
+		}
+
+		//Send the list of spies to the players
+		for(i = 0; i < spies.length; i++)
+		{
+			spies[i].socket.emit('chat message', 'TheGame', msg);
+		}
+
+		game.state++;
 	}}; 
 
 	var teamLeader = {name: "teamLeader", func: function(game){
 
+		if(game.teamLeader == -1 )
+		{
+			game.teamLeader = Math.floor((Math.random() * game.players.length));
+		}
+		else
+		{
+			game.teamLeader++;
+
+			if(game.teamLeader >= game.players.length)
+			{
+				game.teamLeader = 0; 
+			}
+		}
+
+		game.players[game.teamLeader].socket.emit('chat message', 'TheGame', 'You are the leader, choose your team');
+
+		game.state++;
 	}};
 
 	var teamBuild = {name: "teamBuild", func: function(game){
 
+		game.state++;
 	}};
 
 	var voteTeam = {name: "voteTeam", func: function(game){
+
 
 	}}; 
 
@@ -96,6 +144,10 @@ var Resistance = function()
 		{
 			case "start game":
 				init.func(this);
+			break;
+
+			case "next state":
+				this.ResistanceGameState[this.state].func(this);
 			break;
 
 			default:
